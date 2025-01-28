ac12a750cd8e65fdb289ede8d7ccb84d1b2447ad
Chris Wyatt <9978862+chriswyatt1@users.noreply.github.com>
2025-01-28 14:08:11 +0000

    Update silhouette_black.py
    Fixed working black background version

--- silhouette_black.py
+++ silhouette_black.py
@@ -1,7 +1,7 @@
 import sys
 from PIL import Image, ImageOps, ImageFilter
 
-def convert_to_silhouette(input_image_path, output_image_path_pattern, threshold_values, invert_colors=False, blur_radius=2):
+def convert_to_silhouette(input_image_path, output_image_path_pattern, threshold_values, blur_radius=2):
     # Load the image
     image = Image.open(input_image_path)
 
@@ -17,11 +17,8 @@ def convert_to_silhouette(input_image_path, output_image_path_pattern, threshold
     
     for threshold_value in threshold_values:
         # Threshold the image to create a binary (black and white) image
-        silhouette = blurred.point(lambda x: 255 if x > threshold_value else 0, '1')  # Keep light (non-black) areas as the silhouette
-
-        # Optionally invert the binary image
-        if invert_colors:
-            silhouette = ImageOps.invert(silhouette)
+        # The silhouette will be black (0) on a white (255) background
+        silhouette = blurred.point(lambda x: 0 if x > threshold_value else 255, '1')
 
         # Generate the output file path
         output_image_path = output_image_path_pattern.format(threshold_value=threshold_value)
@@ -32,7 +29,6 @@ def convert_to_silhouette(input_image_path, output_image_path_pattern, threshold
 # Example usage
 input_image_path = sys.argv[1]
 output_image_path_pattern = "Silhouette_{threshold_value}.png"
-threshold_values = [10, 20, 30, 50, 75, 100, 150, 200] # List of threshold values to generate different outputs
-invert_colors = False  # Set to False for black background
+threshold_values = [10, 20, 30, 50, 75, 100, 150, 200]  # List of threshold values to generate different outputs
 
-convert_to_silhouette(input_image_path, output_image_path_pattern, threshold_values, invert_colors)
+convert_to_silhouette(input_image_path, output_image_path_pattern, threshold_values)
